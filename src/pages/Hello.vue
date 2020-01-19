a87a97cd1c82a9ccc1cf755989ad150f66fde7cf
krengt <krengt@live.jp>
2020-01-19 20:50:11 +0900

    Initial commit

---
+++ src/pages/Hello.vue
@@ -0,0 +1,17 @@
+<template>
+  <Layout>
+    <h1>Hello Gridsome</h1>
+  </Layout>
+</template>
+
+<script>
+export default {
+  name: 'hello'
+}
+</script>
+
+<style scoped>
+h1 {
+  color: sienna;
+}
+</style>
